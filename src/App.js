c07a9fc10d91b58d7fdd7795c645f807e661818d
Sara Yazmin Espinal Araque <sara.espinala@perficient.com>
2020-10-23 11:45:24 -0500

    initial form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { Form } from './components';
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/url-random-code/:code" >
+          <Form />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
